223822bed3bffc53e8e2bf00356ed8f3524fd355
al-metal <al-metal@bk.ru>
2017-03-03 10:00:21 +0400

    Убрал из игнора layout

---
+++ app/src/main/java/com/vortex/vortex/APK/ActivityApkMoySredstvaVoda.java
@@ -0,0 +1,15 @@
+package com.vortex.vortex.APK;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vortex.vortex.R;
+
+public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apk_moy_sredstva_voda);
+    }
+}
