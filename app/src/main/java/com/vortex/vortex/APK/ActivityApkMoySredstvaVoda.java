0a31c708e21882a58894bcac070857644fa5299b
al-metal <al-metal@bk.ru>
2017-03-03 16:43:22 +0400

    Вывод жесткости воды

--- app/src/main/java/com/vortex/vortex/APK/ActivityApkMoySredstvaVoda.java
+++ app/src/main/java/com/vortex/vortex/APK/ActivityApkMoySredstvaVoda.java
@@ -14,9 +14,13 @@ import android.widget.Toast;
 import com.vortex.vortex.R;
 
 public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
-    
+
     private TextView tvDh;
+    private TextView tvDh2;
+    private TextView tvVoda;
     private EditText etVoda;
+
+    private double voda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,27 +28,45 @@ public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
         setContentView(R.layout.activity_apk_moy_sredstva_voda);
         setTitle("Подбор средства под жесткость воды");
         tvDh = (TextView) findViewById(R.id.tvDh);
+        tvDh2 = (TextView) findViewById(R.id.tvDh2);
+        tvVoda = (TextView) findViewById(R.id.tvVoda);
+        etVoda = (EditText) findViewById(R.id.etVoda);
     }
 
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
+        if(etVoda.getText().length() != 0)
+        voda = Double.parseDouble(etVoda.getText().toString());
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.rbDjeskost:
                 if (checked){
                     tvDh.setText("°Ж");
+                    tvDh2.setText("°Ж");
+                    if(etVoda.getText().length() != 0){
+                        tvVoda.setText(etVoda.getText().toString());
+                    }
                 }
                     break;
             case R.id.rbDh:
                 if (checked){
                     tvDh.setText("°DH");
+                    tvDh2.setText("°DH");
+                    if(etVoda.getText().length() != 0){
+                        voda =  voda*0.36;
+                        tvVoda.setText(String.valueOf(voda));
+                    }
                 }
                     break;
             case R.id.rbMgL:
                 if (checked){
                     tvDh.setText("мг - экв/л");
+                    tvDh2.setText("мг - экв/л");
+                    if(etVoda.getText().length() != 0){
+                        tvVoda.setText(etVoda.getText().toString());
+                    }
                 }
                     break;
         }
