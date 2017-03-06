bf0f439a17e44433343f8438de7ed9b29d913bd1
al-metal <al-metal@bk.ru>
2017-03-06 16:04:44 +0400

    Вывод рекомендованных средств
    Кривой дизайн

--- app/src/main/java/com/vortex/vortex/APK/ActivityApkMoySredstvaVoda.java
+++ app/src/main/java/com/vortex/vortex/APK/ActivityApkMoySredstvaVoda.java
@@ -27,6 +27,15 @@ public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
     private RadioButton rbDh;
     private RadioButton rbMgL;
 
+    private TextView tvSheloch1;
+    private TextView tvKislot1;
+    private TextView tvSheloch2;
+    private TextView tvKislot2;
+    private TextView tvSheloch3;
+    private TextView tvKislot3;
+    private TextView tvSheloch4;
+    private TextView tvKislot4;
+
     private double voda;
 
     @Override
@@ -41,6 +50,15 @@ public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
         rbDjeskost = (RadioButton) findViewById(R.id.rbDjeskost);
         rbMgL = (RadioButton) findViewById(R.id.rbMgL);
         rbDh = (RadioButton) findViewById(R.id.rbDh);
+
+        tvSheloch1 = (TextView) findViewById(R.id.tvSheloch1);
+        tvKislot1 = (TextView) findViewById(R.id.tvKislot1);
+        tvSheloch2 = (TextView) findViewById(R.id.tvSheloch2);
+        tvKislot2 = (TextView) findViewById(R.id.tvKislot2);
+        tvSheloch3 = (TextView) findViewById(R.id.tvSheloch3);
+        tvKislot3 = (TextView) findViewById(R.id.tvKislot3);
+        tvSheloch4 = (TextView) findViewById(R.id.tvSheloch4);
+        tvKislot4 = (TextView) findViewById(R.id.tvKislot4);
 
         ((EditText) findViewById(R.id.etVoda)).addTextChangedListener(new TextWatcher() {
 
@@ -155,4 +173,115 @@ public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
         return new BigDecimal(""+value).setScale(digits, BigDecimal.ROUND_HALF_UP);
     }
 
+    public void onClickRaschetRekomendSredstv(View view) {
+        double voda = Double.parseDouble(tvVoda.getText().toString());
+        String biotekC = "";
+        String ksilanK = "";
+        String biotek = "";
+        String ksilan = "";
+        String biotekM = "";
+        String ksilanM = "";
+        String biotekSuper = "";
+        String ksilanSuper = "";
+
+        if (voda <= 1)
+        {
+            biotekM = "Biotec M, 0,4%";
+            ksilanM = "Ksilan M, 0,4%";
+        }
+        else if (voda <= 2)
+        {
+            biotekM = "Biotec M, 0,5%";
+            ksilanM = "Ksilan M, 0,5%";
+        }
+        else if (voda <= 3)
+        {
+            biotekM = "Biotec M, 0,7%";
+            ksilanM = "Ksilan M, 0,7%";
+        }
+        else if (voda <= 4)
+        {
+            biotekM = "Biotec M, 1%";
+            ksilanM = "Ksilan M, 1%";
+        }
+        else
+        {
+            biotekM = "Biotec M не используется";
+            ksilanM = "Ksilan M не используется";
+        }
+
+
+
+
+        if (voda <= 5)
+        {
+            biotekC = "Biotec C, 0,3%";
+            ksilanK = "Ksilan K, 0,3%";
+        }
+        else if (voda <= 6.5)
+        {
+            biotekC = "Biotec C, 0,5%";
+            ksilanK = "Ksilan K, 0,5%";
+        }
+        else if (voda <= 8)
+        {
+            biotekC = "Biotec C, 1%";
+            ksilanK = "Ksilan K, 1%";
+        }
+        else
+        {
+            biotekC = "Biotec C не используется";
+            ksilanK = "Ksilan K не используется";
+        }
+
+
+
+        if (voda <= 5)
+        {
+            biotek = "Biotec, 0,3%";
+            ksilan = "Ksilan, 0,3%";
+        }
+        else if (voda <= 6.5)
+        {
+            biotek = "Biotec, 0,5%";
+            ksilan = "Ksilan, 0,5%";
+        }
+        else if (voda <= 8)
+        {
+            biotek = "Biotec, 0,7%";
+            ksilan = "Ksilan, 0,7%";
+        }
+        else
+        {
+            biotek = "Biotec не используется";
+            ksilan = "Ksilan не используется";
+        }
+
+
+
+        if (voda <= 10)
+        {
+            biotekSuper = "Biotec Super, 0,3%";
+            ksilanSuper = "Ksilan Super, 0,3%";
+        }
+        else if (voda <= 12)
+        {
+            biotekSuper = "Biotec Super, 0,4%";
+            ksilanSuper = "Ksilan Super, 0,4%";
+        }
+        else
+        {
+            biotekSuper = "Biotec Super не используется";
+            ksilanSuper = "Ksilan Super не используется";
+        }
+
+        tvSheloch1.setText(biotekM);
+        tvKislot1.setText(ksilanM);
+        tvSheloch2.setText(biotekC);
+        tvKislot2.setText(ksilanK);
+        tvSheloch3.setText(biotek);
+        tvKislot3.setText(ksilan);
+        tvSheloch4.setText(biotekSuper);
+        tvKislot4.setText(ksilanSuper);
+    }
 }
