4a7d214b54314120c1bf0e2347009294dd8facd5
al-metal <al-metal@bk.ru>
2017-03-06 09:58:00 +0400

    Вывод жесткости воды

--- app/src/main/java/com/vortex/vortex/APK/ActivityApkMoySredstvaVoda.java
+++ app/src/main/java/com/vortex/vortex/APK/ActivityApkMoySredstvaVoda.java
@@ -3,6 +3,8 @@ package com.vortex.vortex.APK;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,12 +15,17 @@ import android.widget.Toast;
 
 import com.vortex.vortex.R;
 
+import java.math.BigDecimal;
+
 public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
 
     private TextView tvDh;
-    private TextView tvDh2;
     private TextView tvVoda;
     private EditText etVoda;
+    private TextView tvVodaStr;
+    private RadioButton rbDjeskost;
+    private RadioButton rbDh;
+    private RadioButton rbMgL;
 
     private double voda;
 
@@ -28,9 +35,80 @@ public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
         setContentView(R.layout.activity_apk_moy_sredstva_voda);
         setTitle("Подбор средства под жесткость воды");
         tvDh = (TextView) findViewById(R.id.tvDh);
-        tvDh2 = (TextView) findViewById(R.id.tvDh2);
         tvVoda = (TextView) findViewById(R.id.tvVoda);
+        tvVodaStr = (TextView) findViewById(R.id.tvVodaStr);
         etVoda = (EditText) findViewById(R.id.etVoda);
+        rbDjeskost = (RadioButton) findViewById(R.id.rbDjeskost);
+        rbMgL = (RadioButton) findViewById(R.id.rbMgL);
+        rbDh = (RadioButton) findViewById(R.id.rbDh);
+
+        ((EditText) findViewById(R.id.etVoda)).addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(etVoda.getText().length() != 0){
+                    voda = Double.parseDouble(etVoda.getText().toString());
+                    if(rbDjeskost.isChecked() == true){
+                        tvVoda.setText(etVoda.getText().toString());
+                        ReturnVoda(tvVoda);
+                    }
+                    else if(rbDh.isChecked() == true){
+                        voda =  voda*0.36;
+                        tvVoda.setText(String.valueOf(roundUp(voda, 2)));
+                        ReturnVoda(tvVoda);
+                    }
+                    else if(rbMgL.isChecked() == true){
+                        tvVoda.setText(etVoda.getText().toString());
+                        ReturnVoda(tvVoda);
+                    }
+                }
+                else {
+                    tvVoda.setText("0");
+                }
+            }
+
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // текст будет изменен
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // текст уже изменили
+            }
+        });
+
+    }
+
+    private void ReturnVoda(TextView tvVoda) {
+        double vod = Double.parseDouble(tvVoda.getText().toString());
+        if (0 <= vod && vod < 1.5)
+        {
+            tvVodaStr.setText("очень мягкая");
+        }
+        else if (1.5 <= vod && vod < 3)
+        {
+            tvVodaStr.setText("мягкая");
+        }
+        else if (3 <= vod && vod < 6)
+        {
+            tvVodaStr.setText("умеренной жесткости");
+        }
+        else if (6 <= vod && vod <= 12)
+        {
+            tvVodaStr.setText("жесткая");
+        }
+        else if (vod > 12)
+        {
+            tvVodaStr.setText("очень жесткая");
+        }
+        else
+        {
+            tvVodaStr.setText("");
+        }
+
     }
 
     public void onRadioButtonClicked(View view) {
@@ -44,33 +122,37 @@ public class ActivityApkMoySredstvaVoda extends AppCompatActivity {
             case R.id.rbDjeskost:
                 if (checked){
                     tvDh.setText("°Ж");
-                    tvDh2.setText("°Ж");
                     if(etVoda.getText().length() != 0){
                         tvVoda.setText(etVoda.getText().toString());
+                        ReturnVoda(tvVoda);
                     }
                 }
                     break;
             case R.id.rbDh:
                 if (checked){
                     tvDh.setText("°DH");
-                    tvDh2.setText("°DH");
                     if(etVoda.getText().length() != 0){
                         voda =  voda*0.36;
-                        tvVoda.setText(String.valueOf(voda));
+                        tvVoda.setText(String.valueOf(roundUp(voda, 2)));
+                        ReturnVoda(tvVoda);
                     }
                 }
                     break;
             case R.id.rbMgL:
                 if (checked){
                     tvDh.setText("мг - экв/л");
-                    tvDh2.setText("мг - экв/л");
                     if(etVoda.getText().length() != 0){
                         tvVoda.setText(etVoda.getText().toString());
+                        ReturnVoda(tvVoda);
                     }
                 }
                     break;
         }
     }
 
+
+    public BigDecimal roundUp(double value, int digits){
+        return new BigDecimal(""+value).setScale(digits, BigDecimal.ROUND_HALF_UP);
+    }
 
 }
