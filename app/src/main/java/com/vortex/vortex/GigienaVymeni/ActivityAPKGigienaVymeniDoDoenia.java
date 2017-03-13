e7d026c39f216c679bf76b8eb98c9c5bffba5438
al-metal <al-metal@bk.ru>
2017-03-13 12:38:08 +0400

    Расчет всех данных гигиена до доения

--- app/src/main/java/com/vortex/vortex/GigienaVymeni/ActivityAPKGigienaVymeniDoDoenia.java
+++ app/src/main/java/com/vortex/vortex/GigienaVymeni/ActivityAPKGigienaVymeniDoDoenia.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -18,9 +19,19 @@ public class ActivityAPKGigienaVymeniDoDoenia extends AppCompatActivity {
     String[] data = {"Приолит", "Виолит", "Фитолит", "Алгалит 50", "Алгалит"};
     String[] data2 = {"10", "15", "20", "30", "40", "100"};
 
+    Spinner spinner;
     Spinner spinner2;
     TextView tvRashodGolova;
     double dblRashodGolova = 0;
+    EditText etPrice;
+    EditText etVes;
+    EditText etKolichGolov;
+    EditText etDay;
+    EditText etKolichObrabotok;
+
+    TextView tvSoimostKg;
+    TextView tvKolichGigien;
+    TextView tvStoimPromyvki;
 
 
     @Override
@@ -29,9 +40,18 @@ public class ActivityAPKGigienaVymeniDoDoenia extends AppCompatActivity {
         setContentView(R.layout.activity_apkgigiena_vymeni_do_doenia);
         setTitle("Гигиена вымени до доения");
 
-        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner = (Spinner) findViewById(R.id.spinner);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
         tvRashodGolova = (TextView) findViewById(R.id.tvRashodGolova);
+        etPrice = (EditText) findViewById(R.id.etPrice);
+        etVes = (EditText) findViewById(R.id.etVes);
+        etKolichGolov = (EditText) findViewById(R.id.etKolichGolov);
+        etDay = (EditText) findViewById(R.id.etDay);
+        etKolichObrabotok = (EditText) findViewById(R.id.etKolichObrabotok);
+
+        tvSoimostKg = (TextView) findViewById(R.id.tvSoimostKg);
+        tvKolichGigien = (TextView) findViewById(R.id.tvKolichGigien);
+        tvStoimPromyvki = (TextView) findViewById(R.id.tvStoimPromyvki);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -74,8 +94,7 @@ public class ActivityAPKGigienaVymeniDoDoenia extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int pos, long id) {
 
-                if (spinner2.getSelectedItemId() == 5)
-                {
+                if (spinner2.getSelectedItemId() == 5) {
                     if (spinner.getSelectedItemId() == 4)
                         spinner2.setSelection(5);
                     else
@@ -94,14 +113,32 @@ public class ActivityAPKGigienaVymeniDoDoenia extends AppCompatActivity {
                 else if (spinner2.getSelectedItemId() == 5)
                     dblRashodGolova = 0.0008 * 4;
 
-                String s = String.valueOf(roundUp(dblRashodGolova,5));
+                String s = String.valueOf(roundUp(dblRashodGolova, 5));
                 tvRashodGolova.setText(s);
             }
         });
-
     }
 
     public BigDecimal roundUp(double value, int digits) {
         return new BigDecimal("" + value).setScale(digits, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public void onClickRaschet(View view) {
+
+        double price = Double.parseDouble(etPrice.getText().toString());
+        double ves = Double.parseDouble(etVes.getText().toString());
+        double kolichGolov = Double.parseDouble(etKolichGolov.getText().toString());
+        double period = Double.parseDouble(etDay.getText().toString());
+        double kolichObrabotok = Double.parseDouble(etKolichObrabotok.getText().toString());
+
+        double stoimKg = price / ves;
+        double kolichGigien = dblRashodGolova*kolichGolov*period*kolichObrabotok;
+        double stoimObrabotki = dblRashodGolova * stoimKg;
+
+        tvSoimostKg.setText(String.valueOf(roundUp(stoimKg, 2)));
+        tvKolichGigien.setText(String.valueOf(roundUp(kolichGigien, 2)));
+        tvStoimPromyvki.setText(String.valueOf(roundUp(stoimObrabotki, 2)));
+
+
     }
 }
