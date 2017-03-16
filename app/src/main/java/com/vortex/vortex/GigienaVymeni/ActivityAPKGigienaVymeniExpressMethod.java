f826391163bcea6de2b9b464329934d829881159
al-metal <al-metal@bk.ru>
2017-03-16 11:35:50 +0400

    Сравненеи средств експресс метод

--- app/src/main/java/com/vortex/vortex/GigienaVymeni/ActivityAPKGigienaVymeniExpressMethod.java
+++ app/src/main/java/com/vortex/vortex/GigienaVymeni/ActivityAPKGigienaVymeniExpressMethod.java
@@ -1,5 +1,6 @@
 package com.vortex.vortex.GigienaVymeni;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.vortex.vortex.ActivityAPKGigienaVymeniExpressMethodSravnenie;
 import com.vortex.vortex.R;
 
 import java.math.BigDecimal;
@@ -20,6 +22,11 @@ public class ActivityAPKGigienaVymeniExpressMethod extends AppCompatActivity {
     TextView tvKolichestvo;
     TextView tvStoimostObrabotkiVsego;
     TextView tvStoimostObrabotkiGolovy;
+
+    double dblStoimKg;
+    double dblKolichestvo;
+    double dblStoimostVsego;
+    double dblStoimostGolovy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,19 +56,31 @@ public class ActivityAPKGigienaVymeniExpressMethod extends AppCompatActivity {
         double ves = Double.parseDouble(etVes.getText().toString());
         double kolichGolov = Double.parseDouble(etKolichGolov.getText().toString());
 
-        double dblStoimKg = price/ves;
-        double dblKolichestvo = kolichGolov*0.008;
-        double dblStoimostVsego = 0.008*dblStoimKg*kolichGolov;
-        double dblStoimostGolovy = dblStoimostVsego/kolichGolov;
+         dblStoimKg = price/ves;
+         dblKolichestvo = kolichGolov*0.008;
+         dblStoimostVsego = 0.008*dblStoimKg*kolichGolov;
+         dblStoimostGolovy = dblStoimostVsego/kolichGolov;
 
         tvStoimKg.setText(String.valueOf(roundUp(dblStoimKg, 2)));
         tvKolichestvo.setText(String.valueOf(roundUp(dblKolichestvo, 2)));
         tvStoimostObrabotkiVsego.setText(String.valueOf(roundUp(dblStoimostVsego, 2)));
         tvStoimostObrabotkiGolovy.setText(String.valueOf(roundUp(dblStoimostGolovy, 2)));
-
     }
 
     public BigDecimal roundUp(double value, int digits) {
         return new BigDecimal("" + value).setScale(digits, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public void onClickSravnenie(View view) {
+        if(dblStoimKg ==0 || dblKolichestvo ==0 || dblStoimostVsego ==0 || dblStoimostGolovy ==0){
+            Toast.makeText(getBaseContext(), "Данные для сравнения еще не расчитаны", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent intent = new Intent(ActivityAPKGigienaVymeniExpressMethod.this, ActivityAPKGigienaVymeniExpressMethodSravnenie.class);
+        intent.putExtra("stoimKg", dblStoimKg);
+        intent.putExtra("kolichestvo", dblKolichestvo);
+        intent.putExtra("stoimostVsego", dblStoimostVsego);
+        intent.putExtra("stoimostGolovy", dblStoimostGolovy);
+        startActivity(intent);
     }
 }
