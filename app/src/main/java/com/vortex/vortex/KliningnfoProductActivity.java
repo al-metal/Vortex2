b3eaf655bd0834d731845eec6336787512581b04
al-metal <al-metal@users.noreply.github.com>
2018-08-22 13:17:08 +0400

    TM CLEANBOX
    Реализована информация о продукте

--- app/src/main/java/com/vortex/vortex/KliningnfoProductActivity.java
+++ app/src/main/java/com/vortex/vortex/KliningnfoProductActivity.java
@@ -1,9 +1,17 @@
 package com.vortex.vortex;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,9 +26,20 @@ import android.widget.TextView;
 public class KliningnfoProductActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
+
+    private String LOG_TAG = "myLogs";
+    private String DB_VERSION;
+    private boolean err;
+
     private String product;
     private TextView tvAppointment;
     private TextView tvInstruction;
+
+    private String appointment;
+    private String instruction;
+    private String strNONE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,17 +64,11 @@ public class KliningnfoProductActivity extends AppCompatActivity
         tvAppointment = findViewById(R.id.tvAppointment);
         tvInstruction = findViewById(R.id.tvInstruction);
 
-        switch (product) {
-            case "DAZE":
-                tvAppointment.setText("Жидкое моющее средство для обработки внешних и внутренних поверхностей технологического оборудования (коптильных камер, жаровен, грилей, духовок, плит, пароконвектоматов и т.д.) от белково-жировых пригаров и смол на предприятиях пищевой, пивной и мясоперерабатывающей промышленностей. Хорошо растворимо в воде. Средство обладает высоким смачивающим, очищающим, эмульгирующим действием. Идеально подходит для обработки с помощью пенообразующего оборудования (пеногенераторы, стационарные и мобильные пенные станции)");
-                tvInstruction.setText("Средство в триггерной упаковке (0,6 кг.) готово к применению, не требует разбавления. Равномерно нанести на обрабатываемую поверхность. Выдержать 1-10 минут. В случае сильных загрязнений потереть губкой или щеткой. Смыть средство водой\n" +
-                        "При использовании продукта в фасовке 1, 3 и 6 кг. концентрация, экспозиция средства подбирается индивидуально, в зависимости от загрязнения.");
-                break;
-            default:
-                tvAppointment.setText("Средство еще не обработано");
-                tvInstruction.setText("Средство еще не обработано");
+        appointment = "";
+        instruction = "";
+        strNONE = "Данные не найдены";
 
-        }
+        new Load_data(this).execute();
     }
 
     @Override
@@ -90,5 +103,100 @@ public class KliningnfoProductActivity extends AppCompatActivity
         Intent intent = new Intent(KliningnfoProductActivity.this, ActivityKliningNewCalculation.class);
         intent.putExtra("nameProduct", product);
         startActivity(intent);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class Load_data extends AsyncTask<Void, Void, Void> {
+
+        Context mContext;
+
+        public Load_data(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Logo.mSettings = getSharedPreferences(Logo.APP_PREFERENCES, Context.MODE_PRIVATE);
+                if (Logo.mSettings.contains(Logo.APP_PREFERENCES_COUNTER)) {
+                    // Получаем число из настроек
+                    DB_VERSION = Logo.mSettings.getString(Logo.APP_PREFERENCES_COUNTER, "");
+                }
+
+                Log.d(LOG_TAG, "--- Версия БД: " + DB_VERSION + " ----");
+
+                mDBHelper = new DatabaseHelper(mContext, DB_VERSION);
+
+                try {
+                    mDb = mDBHelper.getWritableDatabase();//.getReadableDatabase();
+                } catch (SQLException mSQLException) {
+                    throw mSQLException;
+                }
+
+                try {
+                    Log.d(LOG_TAG, "--- Получение данных из БД cleanbox" + " ----");
+                    Cursor c = mDb.query("cleanboxInfoProduct", null, null, null, null, null, null);
+
+                    // ставим позицию курсора на первую строку выборки
+                    // если в выборке нет строк, вернется false
+                    if (c.moveToFirst()) {
+
+                        // определяем номера столбцов по имени в выборке
+                        int idColIndex = c.getColumnIndex("id");
+                        int nameColIndex = c.getColumnIndex("name");
+                        int appointmentColIndex = c.getColumnIndex("appointment");
+                        int instructionColIndex = c.getColumnIndex("instruction");
+
+                        do {
+                            String product_db = c.getString(nameColIndex);
+                            if (product.equals(product_db)) {
+
+                                appointment = c.getString(appointmentColIndex);
+                                instruction = c.getString(instructionColIndex);
+                            }
+                        } while (c.moveToNext());
+                    } else
+                        Log.d(LOG_TAG, "0 rows");
+                    c.close();
+                } catch (Exception e) {
+                    err = true;
+                    Log.d(LOG_TAG, "--- Ошибка " + e + " ----");
+                }
+
+            } catch (Exception e) {
+                err = true;
+            }
+            return null;
+        }
+
+        @SuppressLint("WrongConstant")
+        protected void onPostExecute(Void result) {
+
+            if (err) {
+                Log.d(LOG_TAG, "--- Ошибка есть ----");
+                AlertDialog.Builder builder = new AlertDialog.Builder(KliningnfoProductActivity.this);
+                builder.setTitle("Ошибка")
+                        .setMessage(getString(R.string.noneDB))
+                        //.setIcon(R.drawable.ic_android_cat)
+                        .setCancelable(false)
+                        .setNegativeButton("ОК",
+                                (dialog, id) -> {
+                                    //btnRaschetMarvel.setEnabled(false);
+                                    dialog.cancel();
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                Log.d(LOG_TAG, "--- Диалог показан ----");
+            } else {
+
+                if(appointment != "" && instruction != "") {
+                    tvAppointment.setText(appointment);
+                    tvInstruction.setText(instruction);
+                }else {
+                    tvAppointment.setText(strNONE);
+                    tvInstruction.setText(strNONE);
+                }
+            }
+        }
     }
 }
