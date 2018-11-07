477d2fa369888aeeae07fe7f318129ccd5c2a5b6
al-metal <al-metal@users.noreply.github.com>
2018-11-07 09:22:05 +0400

    Оптимизация кода новостей

--- app/src/main/java/com/vortex/vortex/news_array2.java
+++ app/src/main/java/com/vortex/vortex/news_array2.java
@@ -2,29 +2,20 @@ package com.vortex.vortex;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -32,30 +23,16 @@ import com.google.gson.Gson;
 import com.vortex.vortex.Adapter.FeedAdapter;
 import com.vortex.vortex.Common.HTTPDataHandler;
 import com.vortex.vortex.Model.RSSObject;
-import com.vortex.vortex.models.newsModel;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class news_array2 extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    ProgressBar pgLoadNews;
-    TextView tvProgressBerText;
-    android.widget.Toolbar toolbar;
-    RecyclerView recyclerView;
-    RSSObject rssObject;
+    private ProgressBar pgLoadNews;
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private RSSObject rssObject;
+
+    private TextView tvProgressBerText;
 
     private final String RSSLink = "https://pk-vortex.ru/news/rss/";
     private final String RSSToJsonApi = "https://api.rss2json.com/v1/api.json?rss_url=";
@@ -64,23 +41,24 @@ public class news_array2 extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_array2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("Новости");
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        tvProgressBerText = (TextView) findViewById(R.id.tvProgressBerText);
-        pgLoadNews = (ProgressBar) findViewById(R.id.pgLoadNews);
+        tvProgressBerText = findViewById(R.id.tvProgressBerText);
+        pgLoadNews = findViewById(R.id.pgLoadNews);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView = findViewById(R.id.recycler);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getBaseContext(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
 
@@ -89,7 +67,7 @@ public class news_array2 extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -110,7 +88,7 @@ public class news_array2 extends AppCompatActivity
         Intent intent = ClickLeftMenu.getIntent(news_array2.this, item);
         startActivity(intent);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
